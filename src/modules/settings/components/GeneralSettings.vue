6f3ef662232ca4b6483a24047b9a858889b3f991
Robin Mons <robin.mons@hotmail.nl>
2024-11-29 17:21:04 +0100

    settings loading animation

--- src/modules/settings/components/GeneralSettings.vue
+++ src/modules/settings/components/GeneralSettings.vue
@@ -16,14 +16,14 @@
     const fullName = ref("");
 
     onMounted(() => {
-        if(!accountState.user) return;
+        if (!accountState.user) return;
 
         email.value = accountState.user.email;
         fullName.value = accountState.user.full_name;
     });
 
     async function selectImage(evt: any) {
-        if(!acceptableTypes.includes(evt.target.files[0].type)) {
+        if (!acceptableTypes.includes(evt.target.files[0].type)) {
             return;
         }
         files.value = evt.target.files;
@@ -35,10 +35,10 @@
 
     async function save() {
         const supabase = await getSupabaseClient();
-        if(imageLink.value && !image.value) {
+        if (imageLink.value && !image.value) {
             return;
         }
-        if(!accountState.user) return;
+        if (!accountState.user) return;
         if (!image.value) return;
         const file = image.value;
         const fileExt = file.name.split('.').pop();
@@ -57,7 +57,7 @@
     }
 
     async function deleteFoto() {
-        if(!accountState.user) return;
+        if (!accountState.user) return;
         const supabase = await getSupabaseClient();
         await supabase.storage.from('user_pictures').remove([accountState.user.picture]);
         accountState.user.picture = "";
@@ -65,8 +65,8 @@
     }
 
     async function saveInput() {
-        if(!accountState.user) return;
-        if(accountState.user.email == email.value && accountState.user.full_name == fullName.value) return;
+        if (!accountState.user) return;
+        if (accountState.user.email == email.value && accountState.user.full_name == fullName.value) return;
         accountState.user.email = email.value;
         accountState.user.full_name = fullName.value
 
@@ -76,26 +76,43 @@
 </script>
 
 <template>
-    <div class="w-full py-8 border-b border-gray-500 border-opacity-10 flex items-center">
-        <div class="w-[350px] pr-10">
-            <p class="text-text-normal font-semibold">Your photo</p>
-            <p class="font-semibold text-sm">This will be displayed on your profile.</p>
+    <div class="general-settings">
+        <div class="w-full py-8 border-b border-gray-500 border-opacity-10 flex items-center ">
+            <div class="w-[350px] pr-10">
+                <p class="text-text-normal font-semibold">Your photo</p>
+                <p class="font-semibold text-sm">This will be displayed on your profile.</p>
+            </div>
+            <div class="flex items-center gap-3 font-semibold">
+                <nuxt-img class="rounded-xl w-16 h-16"
+                          :src="accountState.user?.picture ? 'https://mfouoyeneddsfujxfjci.supabase.co/storage/v1/object/public/user_pictures/' + accountState.user.picture : user.userInfo.picture"></nuxt-img>
+                <input ref="fileInput" type="file" accept="image/*" @change="selectImage" style="display: none"/>
+                <button :disabled="!accountState.user?.picture" class="hover:underline transition duration-200"
+                        @click="deleteFoto">Delete
+                </button>
+                <button @click="fileInput.click()" class="text-blue-700 hover:text-blue-900 transition duration-200">
+                    Update
+                </button>
+            </div>
         </div>
-        <div class="flex items-center gap-3 font-semibold">
-            <nuxt-img class="rounded-xl w-16 h-16" :src="accountState.user?.picture ? 'https://mfouoyeneddsfujxfjci.supabase.co/storage/v1/object/public/user_pictures/' + accountState.user.picture : user.userInfo.picture"></nuxt-img>
-            <input ref="fileInput" type="file" accept="image/*" @change="selectImage" style="display: none"/>
-            <button :disabled="!accountState.user?.picture" class="hover:underline transition duration-200" @click="deleteFoto">Delete</button>
-            <button @click="fileInput.click()" class="text-blue-700 hover:text-blue-900 transition duration-200">Update</button>
-        </div>
-    </div>
-    <div class="w-full py-3 border-b border-gray-500 border-opacity-10 flex flex-col justify-center gap-3">
-        <div class="flex items-center w-full">
-            <p class="text-text-normal font-semibold w-[350px]">Username</p>
-            <input @focusout="saveInput" v-model="fullName" class="flex flex-grow shadow px-6 py-2 rounded-3xl border border-black border-opacity-20" type="text"/>
-        </div>
-        <div class="flex items-center w-full">
-            <p class="text-text-normal font-semibold w-[350px]">Email</p>
-            <input @focusout="saveInput" v-model="email" class="flex flex-grow shadow px-6 py-2 rounded-3xl border border-black border-opacity-20" type="text"/>
+        <div class="w-full py-3 border-b border-gray-500 border-opacity-10 flex flex-col justify-center gap-3">
+            <div class="flex items-center w-full">
+                <p class="text-text-normal font-semibold w-[350px]">Username</p>
+                <input @focusout="saveInput" v-model="fullName"
+                       class="flex flex-grow shadow px-6 py-2 rounded-3xl border border-black border-opacity-20"
+                       type="text"/>
+            </div>
+            <div class="flex items-center w-full">
+                <p class="text-text-normal font-semibold w-[350px]">Email</p>
+                <input @focusout="saveInput" v-model="email"
+                       class="flex flex-grow shadow px-6 py-2 rounded-3xl border border-black border-opacity-20"
+                       type="text"/>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+.general-settings {
+    animation: 1s fadeInUp;
+}
+</style>
